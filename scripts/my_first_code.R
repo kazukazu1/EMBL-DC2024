7fe7d81f5c4b3f521c21fd93e046ce33501d628a
unknown <kueda@ualberta.ca>
2024-01-23 07:01:37 -0700

    this is my first R script

---
+++ scripts/my_first_code.R
@@ -0,0 +1,32 @@
+3 + 4
+3+4
+12/7
+
+weight_kg <- 55
+
+#Use Alt + dash(-) button for assign operator (<-) shortcut
+#weight_kg is now an object
+#Object/variable names cannot start with a number
+#Try to avoid (.) dots in the object name
+
+Weight_Kg <- 34
+
+my_weight_kg <- 50
+(my_weight_kg <- 50)
+(My_weight_kg <- 40)
+
+#you can add parentheses during variable assignment
+#to output and cofirm the value assgined to the variable
+
+2.2 * weight_kg
+weight_kg <- 57
+2.2*weight_kg
+
+weight_lb <- 2.2 * weight_kg
+weight_kg <- 100
+
+
+
+
+
+
